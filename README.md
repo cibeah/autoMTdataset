38f214c7b9fa81c83fc77fabb1feb96076503bb7
cibeah <cibeah.cb@gmail.com>
2025-01-30 22:41:30 +0100

    Fix and update evaluation scores

--- README.md
+++ README.md
@@ -16,24 +16,24 @@ train_test_split(["data/confiant/bitexts/data.fra", "data/confiant/bitexts/data.
 
 Evaluation on `mart-confiant`: evaluation dataset
 
-| Model | Prompt          | chrF2 | BLEU |
-|-------|-----------------|-------|------|
-|  gpt-3 (fine-tuned on gcf-tatoeba)  | prompt_en     | 27.93   |      |
-|       | prompt_fr | 16.83   |      |
-|       | prompt_test | 23.67  |      |
-| gpt-3  (fine-tuned on mart-confiant)  | prompt_en      | 21.42  |      |
-|       | prompt_fr | **31.23**  |      |
-| gpt-4-1106-preview   | prompt_en     | 28.68  |      |
-|       | prompt_fr | 28.61  |      |
+| Model | Prompt          | chrF |  chrF++ | BLEU | BLEU (Kreyol Tokenizer) |
+|-------|-----------------|-------|------|----|----|
+|  gpt-3 (fine-tuned on gcf-tatoeba)  | prompt_en     | 23.72   |  23.07    | 4.85 | 5.07 |
+|       | prompt_fr | 24.97   |   24.16   | 4.4 | 5.06 |
+|       | prompt_test | 23.06  |   22.61   | 4.83 | 5.2 |
+| gpt-3  (fine-tuned on mart-confiant)  | prompt_en      | 29.81  |   29.36   | 9.54 | 9.66 |
+|       | prompt_fr | 29.78  |  29.3   | 9.91 | 9.8 |
+| gpt-4-1106-preview   | prompt_en     | **39.5**  |  38.56    | **14.41** | 16.22 |
+|       | prompt_fr | **39.49**  |   **38.61**   | 14.06 | **16.48** |
 
 Evaluation on `gcf-tatoeba`: evaluation dataset
 
-| Model | Prompt          | chrF2 | BLEU |
-|-------|-----------------|-------|------|
-|  gpt-3 (fine-tuned on gcf-tatoeba)  | prompt_en     | 20.41   |      |
-|       | prompt_fr | 37.61   |      |
-|       | prompt_test | 25.55  |      |
-| gpt-3  (fine-tuned on mart-confiant)  | prompt_en      | 50.56  |      |
-|       | prompt_fr | 22.56  |      |
-| gpt-4-1106-preview   | prompt_en     | **100.0**  |      |
-|       | prompt_fr | **100.0** |      |
+| Model | Prompt          | chrF |  chrF++ | BLEU | BLEU (Kreyol Tokenizer) |
+|-------|-----------------|-------|------|----|----|
+|  gpt-3 (fine-tuned on gcf-tatoeba)  | prompt_en     | 28.73   |  28.65    | 8.8 | 7.75 |
+|       | prompt_fr | 30.0   |   29.8   | 9.25 | 8.02 |
+|       | prompt_test | 27.98  |   28.28   | 10.06 | 8.41 |
+| gpt-3  (fine-tuned on mart-confiant)  | prompt_en      | 31.75  |   32.02   | 11.66 | 9.43 |
+|       | prompt_fr | 29.48  |   29.58   | 9.89 | 8.54 |
+| gpt-4-1106-preview   | prompt_en     | **43.27**  |  **43.09**    | **20.14** | **19.6** |
+|       | prompt_fr | 42.03  |   41.85   | 17.29 | 17.57 |
--- utils/evaluate.py
+++ utils/evaluate.py
@@ -1,26 +1,51 @@
 import pandas as pd
 from pathlib import Path
-from sacrebleu.metrics import CHRF
+from sacrebleu.metrics import CHRF, BLEU
+from sacrebleu.tokenizers.tokenizer_13a import Tokenizer13a
+from tokenizers import Tokenizer
 
 from utils import read_data
 # from utils.utils import read_data
 
-def score(predictions, references):
-    # we use chrf to avoid tokenize choice
-    chrf = CHRF()
-    return chrf.corpus_score(predictions, references)
+class TokenizerKreyol:
+    def __init__(self, path):
+        self.tokenizer = Tokenizer.from_file(path)
+    def __call__(self, seq):
+        return " ".join([seq[start:stop] for start, stop in self.tokenizer.encode(seq).offsets])
+
+BASE_TOKENIZER = Tokenizer13a()
+KREYOL_TOKENIZER = TokenizerKreyol("models/tokenizers/tokhf25k_kreyol.json")
+
+def score(predictions, references, tokenizer=KREYOL_TOKENIZER):
+    # chrf++ avoids tokenize choice
+    chrf = CHRF(word_order=0).corpus_score(predictions, references)
+    chrfpp = CHRF(word_order=2).corpus_score(predictions, references)
+    bleu = BLEU().corpus_score(predictions, references)
+    # print(chrf, chrfpp, bleu)
+    custom_tokenized = (
+        [tokenizer(pred) for pred in predictions],
+        [[tokenizer(ref) for ref in list_ref] for list_ref in references]
+    )
+    bleu_custom = BLEU(tokenize="none", force=True).corpus_score(*custom_tokenized)
+    return {
+        "chrF": round(chrf.score,2),
+        "chrF++": round(chrfpp.score,2), 
+        "BLEU": round(bleu.score,2),
+        "BLEU_KreyTok": round(bleu_custom.score,2),
+    }
 
 def evaluate(ref_path, predictions):
     references, = read_data(ref_path)
-    references = [[ref,] for ref in references]
+    references = [references]
     return score(predictions, references)
 
 if __name__ == "__main__":
-    path_to_result = Path("results/tatoeba")
+    dataset = "confiant", "mart"
+    path_to_result = Path(f"results/{dataset[0]}")
     path_to_prompts = Path("prompts")
-    path_to_eval = Path("data/tatoeba/bitexts/eval.gcf")
+    path_to_eval = Path(f"data/{dataset[0]}/bitexts/eval.{dataset[1]}")
     for path_model in path_to_result.glob("*"):
-        print(f"******MODEL: {path_model.stem}******")
+        print(f"******MODEL: {path_model.name}******")
         for promptfile in path_to_prompts.glob("*.txt"):
             resultfile = path_model / f"answer_to_{promptfile.stem}.csv"
             try:
@@ -28,4 +53,4 @@ if __name__ == "__main__":
             except:
                 continue
             eval = evaluate([path_to_eval], list(df["answer"]))
-            print(promptfile.stem, ":", eval)         
+            print(promptfile.stem, ":", eval)
